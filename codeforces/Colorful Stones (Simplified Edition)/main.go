bf9ec2df40dd894a862905806b8b12e8a6c8e215
kuckjwi <rnrghks09@gmail.com>
2023-09-05 11:58:04 +0900

    remove unnecessary logic

--- codeforces/Colorful Stones (Simplified Edition)/main.go
+++ codeforces/Colorful Stones (Simplified Edition)/main.go
@@ -8,14 +8,12 @@ func main() {
 	_, _ = fmt.Scan(&s, &t)
 
 	j := 0
-	count := 0
 
 	for i := 0; i < len(t); i++ {
 		if t[i] == s[j] {
-			count++
 			j++
 		}
 	}
 
-	fmt.Println(count + 1)
+	fmt.Println(j + 1)
 }
